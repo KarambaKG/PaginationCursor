6392888a230707420ddabe456b8776ba0aa40b78
KarambaKG <karamba92@mail.ru>
2020-05-14 14:22:12 +0600

    filled in controllers

--- app/controllers/api/v1/facts_controller.rb
+++ app/controllers/api/v1/facts_controller.rb
@@ -1,2 +1,49 @@
 class Api::V1::FactsController < ApplicationController
+  before_action :find_fact, only: [:show, :update, :destroy]
+
+  def index
+    @facts = Facts.all
+    render json: @facts
+  end
+
+  def show
+    render json: @fact
+  end
+
+  def create
+    @fact = Fact.find(fact_params)
+    if @fact.save
+      render json: @fact
+    else
+      render error: {error: 'Unable to create Fact.'}, status: 400
+    end
+  end
+
+  def update
+    if @fact
+      @fact.update(fact_params)
+      render json: {message: 'Fact successfully updated.'}, status: 200
+    else
+      render json: {error: 'Unable to update Fact.'}, status: 400
+    end
+  end
+
+  def destroy
+    if @fact
+      @fact.destroy
+      render json: {message: 'Fact successfully destroyed.'}, status: 200
+    else
+      render json: {error: 'Unable to delete Fact.'}, status: 400
+    end
+  end
+
+  private
+
+  def fact_params
+    params.require(:fact).permit(:fact, :likes, :user_id)
+  end
+
+  def find_fact
+    @fact = Fact.find(params[:id])
+  end
 end
